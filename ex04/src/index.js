9da32958ceccaf7f94f70dcf84d2c2f9ad6f3821
halidguid <halidhosi@gmail.com>
2021-03-20 00:31:50 +0100

    opalaa

---
+++ ex04/src/index.js
@@ -0,0 +1,21 @@
+function sumFibbonaci(num) {
+    var fib0 = 0;
+    var fib1 = 1;
+    var fib = 1;
+    var sum = fib0;
+    while (fib <= num) {
+      if (fib % 2) {
+        sum += fib1;
+      }
+      fib = fib0 + fib1;
+      fib1 += fib0;
+      fib0 = fib1 - fib0;
+    }
+    return sum;
+  }
+  console.log(sumFibbonaci(1)); // Change this line
+  console.log(sumFibbonaci(10)); // Change this line
+  console.log(sumFibbonaci(20));
+  console.log(sumFibbonaci(20));
+  console.log(sumFibbonaci(-5)); // Change this line
+  module.exports = sumFibbonaci;
